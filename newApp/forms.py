6c171d83bfe842a1f5864ab3fbc94f2c1b16d0b9
Meri Saarinen <mtmsaa@utu.fi>
2018-10-11 22:32:42 +0300

    fixing

---
+++ newApp/forms.py
@@ -0,0 +1,79 @@
+from django import forms
+<<<<<<< HEAD
+from .models import BlogModel
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+=======
+from .models import BlogModel, User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+>>>>>>> 9de138323cfce2e4640dc640cbdd4acc0774de2f
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = BlogModel
+        fields = ['title', 'body']
+
+class CopyOfForm(forms.Form):
+    CHOICES = [(x,x) for x in ("yes", "no")]
+    option = forms.ChoiceField(choices=CHOICES)
+    c_title = forms.CharField(widget=forms.HiddenInput())
+    c_body = forms.CharField(widget=forms.HiddenInput())
+
+class UserCreationForm(forms.Form):
+    username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
+    email = forms.EmailField(label='Enter email')
+    password = forms.CharField(label='Enter password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise ValidationError("Username already exists")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise ValidationError("Email already exists")
+        return email
+
+    def clean_password2(self):
+<<<<<<< HEAD
+        password = self.cleaned_data.get('password')
+=======
+        password = self.cleaned_data.get('password1')
+>>>>>>> 9de138323cfce2e4640dc640cbdd4acc0774de2f
+        password2 = self.cleaned_data.get('password2')
+        if password and password2 and password != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+<<<<<<< HEAD
+            self.cleaned_data['password']
+=======
+            self.cleaned_data['password1']
+>>>>>>> 9de138323cfce2e4640dc640cbdd4acc0774de2f
+        )
+        return user
